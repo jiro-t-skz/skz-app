7d86fe3bc99501a99cddc0a3a521ee1dc3dcc6d4
jiro-t-skz <>
2025-11-30 23:11:08 +0900

    トレード用のテーブルを新規作成。

---
+++ mvp-app/database/migrations/2025_11_30_052242_create_trade_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trade_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('body');
+            $table->string('type')->nullable();
+            $table->string('target')->nullable();
+            $table->string('contact_info')->nullable();
+            $table->date('date')->nullable();
+            $table->string('place')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trade_posts');
+    }
+};
