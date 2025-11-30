67f1b4e283a91225ab75303eca49f6109769587a
jiro-t-skz <>
2025-11-30 23:12:53 +0900

    トレード投稿用のモデルを新規作成

---
+++ mvp-app/app/Models/TradePost.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TradePost extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'type',
+        'target',
+        'contact_info',
+        'date',
+        'place',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
